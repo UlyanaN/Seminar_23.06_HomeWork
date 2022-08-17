55aca4c2ea8bf9db633eeff1a302e3e78062210a
Ulya <Gerony690@yandex.ru>
2022-08-17 13:37:16 +0300

    Task_19

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -4,12 +4,26 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numst = Convert.ToString (num);
+// if (numst.Length > 2)
+// {
+//     Console.WriteLine ($"Третья цифра: {numst[2]}");
+// }
+// else
+// Console.WriteLine ("Третьей цифры нет");
+
 Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-string numst = Convert.ToString (num);
-if (numst.Length > 2)
-{
-    Console.WriteLine ($"Третья цифра: {numst[2]}");
-}
-else
+int num1 = Convert.ToInt32 (Console.ReadLine());
+if (num1 < 99)
 Console.WriteLine ("Третьей цифры нет");
+else
+{
+    while (num1 > 1000)
+    {
+        num1 = num1 / 10;
+    }
+    int thirdDigit = num1 % 10;
+    Console.WriteLine ($"Третья цифра: {thirdDigit}");
+}
