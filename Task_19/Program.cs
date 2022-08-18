c7ab0a8bc699b020c5b9f771aa981fbc928c2231
Ulya <Gerony690@yandex.ru>
2022-08-18 15:17:31 +0300

    Task_21

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -5,10 +5,26 @@
 // 23432 -> да
 
 Console.WriteLine("Введите пятизначное число: ");
-int num1 = Convert.ToInt32 (Console.ReadLine());
-if (num1 < 9999)
-Console.WriteLine("Число не является пятизначным ");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num < 9999)
+    Console.WriteLine("Число не является пятизначным ");
 else
 {
-    
+    int Num1 = Num / 10000;
+    int Num5 = Num % 10;
+    Num = Num - Num1 * 10000;
+    int Num2 = Num / 1000;
+    int Num4 = (Num / 10) % 10;
+    if (Num1 == Num5)
+    {
+        if (Num2 == Num4)
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        Console.WriteLine("Число не является палиндромом ");
+    }
+    else
+    Console.WriteLine("Число не является палиндромом ");
+
 }
