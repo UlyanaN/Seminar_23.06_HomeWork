712a8882c355e4dc950b65cc357a8193c683498c
Ulya <Gerony690@yandex.ru>
2022-08-19 22:39:36 +0300

    Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int NumofEvenNums(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i] + "]");
+    }
+}
+int[] array = CreateArray (4, 100, 1000);
+// int[] array = { 345, 897, 568, 234, 364, 852 };
+PrintArray(array);
+int res = NumofEvenNums(array);
+Console.WriteLine();
+Console.WriteLine(res);
