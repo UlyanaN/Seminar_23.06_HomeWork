78a2f6b17e2905591e57c37c571f354c3840cd50
Ulya <Gerony690@yandex.ru>
2022-08-25 10:40:57 +0300

    Task_50

---
+++ Task_50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+Console.Clear();
+Console.WriteLine("Введите позиции двумерного массива: ");
+Console.WriteLine("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} | ");
+            else Console.Write($"{arr[i, j],3} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElenent(int[,] arr1, int num1, int num2)
+{
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr1.GetLength(1); j++)
+        {
+            if (i == num1 && j == num2)
+                {
+                    Console.WriteLine($"Значение элемента = {arr1[i, j]}");
+                }
+                else Console.WriteLine("Такого элемента не существует");
+                return;
+                
+        }
+    }
+    return;
+}
+
+int[,] arrayResult = CreateMatrixRndInt(3, 4, 1, 20);
+PrintMatrix(arrayResult);
+FindElenent(arrayResult, number1, number2);
