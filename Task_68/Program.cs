a3746dc30c05d3aec23238f27a4719ff6685c1b0
Ulya <Gerony690@yandex.ru>
2022-08-27 16:04:57 +0300

    Task_68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -2,3 +2,23 @@
 // Даны два неотрицательных числа m и n.
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int AkkFun(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else if (num1 > 0 && num2 == 0)
+        return AkkFun(num1 - 1, n = 1);
+    else if ((num1 > 0) && (num2 > 0))
+        return AkkFun(num1 - 1, AkkFun(num1, num2 - 1));
+    else
+        return num2 + 1;
+}
+
+int res = AkkFun(m, n);
+Console.WriteLine(res);
